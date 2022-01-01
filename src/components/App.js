d91685607a00b5e7f976e819a5e40c4951b2f21f
GeenccMustafa <mustafageencc@gmail.com>
2022-01-01 03:23:17 +0100

    first content added

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header.js";
+import Note from "./Note.js";
+import Footer from "./Footer";
+
+function App() {
+    return( 
+    <div>
+        <Header/>
+        <Note></Note>
+        <Footer></Footer>
+    </div>
+    )}
+
+export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (
+    <div className="note">
+        <h1> This is this note title</h1>
+        <p1> This is the note content</p1>	
+    </div>
+    )};
+
+export default Note;
